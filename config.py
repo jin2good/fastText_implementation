55224cc6417a3cb9a3c7d2efe3ce7459ab5c8a5c
Sean Yi / 이석원 <seantuokc@gmail.com>
2019-11-11 21:13:42 +0900

    Removed --use_preprocessed.

--- config.py
+++ config.py
@@ -9,7 +9,4 @@ def get_args():
 	argp.add_argument('--data_dir', type=str, default='./data')
 	argp.add_argument('--file_prefix', type=str, default='ag_')
 
-	# Process arguments.
-	argp.add_argument('--use_preprocessed', action='store_true', default=False)
-
 	return argp.parse_args()
